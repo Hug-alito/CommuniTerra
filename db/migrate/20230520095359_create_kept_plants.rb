da000682e2476cdf8d4f1d683ab662db8c2b9226
Tommy <sasida@hotmail.fr>
2023-05-20 12:36:18 +0200

    bug fix migration

--- db/migrate/20230520095359_create_kept_plants.rb
+++ db/migrate/20230520095359_create_kept_plants.rb
@@ -2,7 +2,7 @@ class CreateKeptPlants < ActiveRecord::Migration[7.0]
   def change
     create_table :kept_plants do |t|
       t.references :user, null: false, foreign_key: true
-      t.references :plant, null: false, foreign_key: true
+      t.references :owned_plant, null: false, foreign_key: true
 
       t.timestamps
     end
