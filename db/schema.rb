89a93ea46465985621ae2513d7dc28e90fe8740d
Hugo Rischmann <hugo.rischmann@me.com>
2023-05-22 10:09:37 +0200

    added dir and files for future assets like css and js

--- db/schema.rb
+++ db/schema.rb
@@ -14,6 +14,11 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_20_104934) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "a_llotments", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "allotment_users", force: :cascade do |t|
     t.bigint "user_id", null: false
     t.bigint "allotment_id", null: false
