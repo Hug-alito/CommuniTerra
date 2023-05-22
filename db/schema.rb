05352bf28644f4cbedd2492e065ca612788e55ec
Hugo Rischmann <hugo.rischmann@me.com>
2023-05-22 13:40:33 +0200

    now db generate with locations and plants

--- db/schema.rb
+++ db/schema.rb
@@ -14,11 +14,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_05_22_081044) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "a_llotments", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
