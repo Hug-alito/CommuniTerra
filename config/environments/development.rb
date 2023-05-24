b6b3a05731881f9967a60699a38a763fe8da1ff0
ezekiel ravidat <ezekiel.ravidat@yahoo.com>
2023-05-24 16:01:31 +0200

    adding tailwinds to rails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -69,4 +69,8 @@ Rails.application.configure do
   # config.action_cable.disable_request_forgery_protection = true
 
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  #tailwinds 
+  config.assets.debug = true
+  
 end
